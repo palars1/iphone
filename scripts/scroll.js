d68adaa8cee14bd59616412858195ca741d80d01
palars1 <93127908+palars1@users.noreply.github.com>
2022-01-23 17:25:53 +0600

    Add files via upload

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -1,27 +1,34 @@
-const links = document.querySelectorAll('.header-menu__item a')
+const scrollFunk = () => {
+  const links = document.querySelectorAll('.header-menu__item a')
+  const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
+  const newArray = [...links, linkCharacteristics]
 
-seamless.polyfill();
 
-links.forEach((element) => {
-  element.addEventListener('click', (event) => {
-    event.preventDefault()
-    
-    const id = element.getAttribute('href').substring(1)
-    const section = document.getElementById(id)    
 
-    if (section) {
-      section.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-      })
-    } else {
-      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
-        behavior: "smooth",
-        block: "center",
-        inline: "center",
-    });
-    }
-    
+  seamless.polyfill();
+
+  newArray.forEach((element) => {
+    element.addEventListener('click', (event) => {
+      event.preventDefault()
+
+      const id = element.getAttribute('href').substring(1)
+      const section = document.getElementById(id)
+
+      if (section) {
+        section.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      } else {
+        seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+          behavior: "smooth",
+          block: "center",
+          inline: "center",
+        });
+      }
+
+    })
+
   })
-  
-})
+}
+scrollFunk()
---
+++ scripts/tabs.js
@@ -0,0 +1,55 @@
+const tabsFunc = () => {
+  const tabs = document.querySelectorAll('.card-detail__change')
+  const tabsTitle = document.querySelector('.card-details__title')
+  const tabsPrise = document.querySelector('.card-details__price')
+  const tabsImage = document.querySelector('.card__image_item')
+
+  const tabOptions = [
+    {
+      name: "Graphite",
+      memory: '128',
+      price: 60000,
+      image: 'img/iPhone-graphite.webp'
+    },
+    {
+      name: "Silver",
+      memory: '256',
+      price: 65000,
+      image: 'img/iPhone-silver.webp'
+    },
+    {
+      name: "Sierra Blue",
+      memory: '512',
+      price: 70000,
+      image: 'img/iPhone-sierra_blue.webp'
+    }
+  ]
+
+  const changeContent = (index) => {
+    tabsTitle.textContent = `Смартфон Apple iPhone 13 Pro ${tabOptions[index].memory}GB ${tabOptions[index].name}`
+    tabsPrise.textContent = `${tabOptions[index].price}₽`
+    tabsImage.setAttribute('src', tabOptions[index].image)
+  }
+
+  const changeActiveTabs = (indexClickedTab) => {
+    tabs.forEach((tab, index) => {
+      tab.classList.remove('active')
+
+      if (index === indexClickedTab) {
+        tab.classList.add('active')
+      }
+    })
+
+    changeContent(indexClickedTab)
+
+  }
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', ()=> {
+      changeActiveTabs(index)
+    })
+
+  })
+  changeContent(0)
+}
+tabsFunc()
