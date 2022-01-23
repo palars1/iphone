7b43edece6285b5e5a9527ee74e1d60a20b561cf
palars1 <93127908+palars1@users.noreply.github.com>
2022-01-23 13:03:17 +0600

    Add files via upload

---
+++ scripts/scroll.js
@@ -0,0 +1,27 @@
+const links = document.querySelectorAll('.header-menu__item a')
+
+seamless.polyfill();
+
+links.forEach((element) => {
+  element.addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    const id = element.getAttribute('href').substring(1)
+    const section = document.getElementById(id)    
+
+    if (section) {
+      section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      })
+    } else {
+      seamless.elementScrollIntoView(document.querySelector("#characteristics"), {
+        behavior: "smooth",
+        block: "center",
+        inline: "center",
+    });
+    }
+    
+  })
+  
+})
