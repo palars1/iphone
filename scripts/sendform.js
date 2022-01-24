cf53f71c4e498f830b0ddac54913921afdf61165
palars1 <93127908+palars1@users.noreply.github.com>
2022-01-24 16:21:39 +0600

    Add files via upload

---
+++ scripts/sendform.js
@@ -0,0 +1,48 @@
+const sendForm = () => {
+  const btnOpenModal = document.querySelector('.card-details__button_delivery')
+  const cardDetailTitle = document.querySelector('.card-details__title')
+  const modal = document.querySelector('.modal')
+  const modalTitle = modal.querySelector('.modal__title')
+  const modalClose = modal.querySelector('.modal__close')
+  const modalForm = modal.querySelector('form')
+
+  
+
+  btnOpenModal.addEventListener('click', () => {
+    modal.style.display = 'flex'
+    modalTitle.textContent = cardDetailTitle.textContent
+  })
+ 
+  modalClose.addEventListener('click', () => {
+    modal.style.display = 'none'
+  })
+
+  modalForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const labels = modal.querySelectorAll('.modal__label')
+
+    const sendMessage = {}
+
+    labels.forEach(label => {
+      const span = label.querySelector('span')
+      const input = label.querySelector('input')
+
+      sendMessage[span.textContent] =input.value
+    })
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify(sendMessage),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(() => {
+        console.log("SEND")
+      })
+
+  })
+
+}
+sendForm()
